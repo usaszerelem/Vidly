994db0770fc139eb47b519d7a864be132c0011ad
Martin Fallenstedt <martin.fallenstedt@gmail.com>
2023-10-18 13:24:18 -0400

    Bootstrap library to format movie view.

--- movies/models.py
+++ movies/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+# The __str__ magic function override is needed so that the genre
+# is correctly displayed when adding a move in the admin panel
+
 class Genre(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Movie(models.Model):
     title = models.CharField(max_length=50)
@@ -12,3 +17,4 @@ class Movie(models.Model):
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default=timezone.now)
+    
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Movies')
+    # Database abstraction APIs:
+
+    # Filtering is also possible like this
+    # Movie.objects.filter(release_year=1985)
+
+    # To get a single movie
+    # Movie.objects.get(id=1)
+
+    # SELECT * FROM movies_movie
+    movies = Movie.objects.all()
+    # output = ', '.join([m.title for m in movies])
+
+    output = render(request, 'movies/index.html', {'movies': movies})
+    return HttpResponse(output)
