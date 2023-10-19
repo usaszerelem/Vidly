f8078e2a18defd4701aa622bc52bb1506fdb5790
Martin Fallenstedt <martin.fallenstedt@gmail.com>
2023-10-19 11:07:57 -0400

    Movie hyperlink to details added.

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Movie
 
 # Create your views here.
@@ -17,5 +17,9 @@ def index(request):
     movies = Movie.objects.all()
     # output = ', '.join([m.title for m in movies])
 
-    output = render(request, 'movies/index.html', {'movies': movies})
-    return HttpResponse(output)
+    return render(request, 'movies/index.html', {'movies': movies})
+
+def detail(request, movie_id):
+    # movie = Movie.objects.get(id=movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
